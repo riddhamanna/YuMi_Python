1ec2df94cb6c39c4f6ddb06b3686f8679f6fe15a
Riddha Manna <riddha.manna@epfl.ch>
2022-07-07 17:04:34 +0200

    codes reformatted

--- YuMiPy.py
+++ YuMiPy.py
@@ -1,31 +1,18 @@
 import socket
 import time
 
-HOST = '192.168.125.1'  # The server's hostname or IP address
-PORT = 4646        # The port used by the server
+HOST = "192.168.125.1"  # The server's hostname or IP address
+PORT = 4646  # The port used by the server
 
 # Declare robtargets and tools according to the targets listed in the 'targets' arrays in the left and right modules in the same order
 
-right_targets = {
-                'vial_1'            :   '1',
-                'vial_hold_closed'  :   '2'
-}
+right_targets = {"vial_1": "1", "vial_hold_closed": "2"}
 
-left_targets = {
-                'plate_on_lcd_1'    :   '1',
-                'bridge_lcd_CO2'    :   '2',
-                'plate_on_CO2'      :   '3'
-}
+left_targets = {"plate_on_lcd_1": "1", "bridge_lcd_CO2": "2", "plate_on_CO2": "3"}
 
-right_tools = {
-                'rGripper'          :   '1',
-                'rSucker'           :   '2'
-}
+right_tools = {"rGripper": "1", "rSucker": "2"}
 
-left_tools = {
-                'lGripper'          :   '1',
-                'lSucker'           :   '2'
-}
+left_tools = {"lGripper": "1", "lSucker": "2"}
 
 # Left specific commands:
 # Lgrip
@@ -40,24 +27,46 @@ left_tools = {
 # Rgrip
 # Rleave
 
+
 def gen_command(action="", action_type="", robot="", tool="", target=""):
-    '''Function to generate string command to send to YuMi'''
+    """Function to generate string command to send to YuMi"""
 
     if robot == "":
-        return(action)
+        return action
     elif robot == "R":
-        return(robot + '_' + action + '_' + action_type + '_' + right_tools[tool]+ '_' + right_targets[target])
+        return (
+            robot
+            + "_"
+            + action
+            + "_"
+            + action_type
+            + "_"
+            + right_tools[tool]
+            + "_"
+            + right_targets[target]
+        )
     elif robot == "L":
-         return(robot + '_' + action + '_' + action_type + '_' + left_tools[tool]+ '_' + left_targets[target])
+        return (
+            robot
+            + "_"
+            + action
+            + "_"
+            + action_type
+            + "_"
+            + left_tools[tool]
+            + "_"
+            + left_targets[target]
+        )
     else:
-        return("error")
+        return "error"
+
 
 def send(comm):
-    '''Function to send string command to YuMi and wait for it to get confirmation from YuMi'''
+    """Function to send string command to YuMi and wait for it to get confirmation from YuMi"""
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.connect((HOST, PORT))
-    s.sendall(bytes(comm,'utf-8'))
+    s.sendall(bytes(comm, "utf-8"))
     data = s.recv(1024)
     time.sleep(0.5)
     s.close()
-    return(str(data))
+    return str(data)
--- demo.py
+++ demo.py
@@ -1,26 +1,6 @@
 import YuMiPy as yp
 import time
 
-
-# commands = ["L_MOVE_L_1", "LgripPlateFromLCD", "L_MOVE_L_2", "L_MOVE_L_3", "LdropPlateOnCO2", "L_MOVE_L_2", "L_MOVE_L_3", "LgripPlateFromCO2", "L_MOVE_L_2", "L_MOVE_L_1", "LdropPlateOnLCD", "L_MOVE_L_2"]
-#
-# for command in commands:
-#     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-#     s.connect((HOST, PORT))
-#     s.sendall(bytes(command,'utf-8'))
-#     data = s.recv(1024)
-#     print('Received', repr(data))
-#     s.close()
-#     time.sleep(1)
-
-# grip in left gripper
-# yp.send(yp.gen_command("Lgrip"))
-# time.sleep(1)
-
-# grip out right gripper
-# yp.send(yp.gen_command("Rleave"))
-# time.sleep(1)
-
 # pick plate 1 from LCD to CO2 pad and back:
 # joint move to bridge position
 # linear move to position of plate 1 on LCD
@@ -35,23 +15,24 @@ import time
 # linear move to LCD plate position 1
 # drop plate on LCD
 # linear move to bridge position
-demo_commands_pickNplace_plate = [["MOVE","J","L","lGripper","bridge_lcd_CO2"],
-                                  ["MOVE","L","L","lGripper","plate_on_lcd_1"],
-                                  ["LgripPlateFromLCD","","","",""],
-                                  ["MOVE","L","L","lGripper","bridge_lcd_CO2"],
-                                  ["MOVE","L","L","lGripper","plate_on_CO2"],
-                                  ["LdropPlateOnCO2","","","",""],
-                                  ["MOVE","L","L","lGripper","bridge_lcd_CO2"],
-                                  ["MOVE","L","L","lGripper","plate_on_CO2"],
-                                  ["LgripPlateFromCO2","","","",""],
-                                  ["MOVE","L","L","lGripper","bridge_lcd_CO2"],
-                                  ["MOVE","L","L","lGripper","plate_on_lcd_1"],
-                                  ["LdropPlateOnLCD","","","",""],
-                                  ["MOVE","L","L","lGripper","bridge_lcd_CO2"],
-                                  ]
+demo_commands_pickNplace_plate = [
+    ["MOVE", "J", "L", "lGripper", "bridge_lcd_CO2"],
+    ["MOVE", "L", "L", "lGripper", "plate_on_lcd_1"],
+    ["LgripPlateFromLCD", "", "", "", ""],
+    ["MOVE", "L", "L", "lGripper", "bridge_lcd_CO2"],
+    ["MOVE", "L", "L", "lGripper", "plate_on_CO2"],
+    ["LdropPlateOnCO2", "", "", "", ""],
+    ["MOVE", "L", "L", "lGripper", "bridge_lcd_CO2"],
+    ["MOVE", "L", "L", "lGripper", "plate_on_CO2"],
+    ["LgripPlateFromCO2", "", "", "", ""],
+    ["MOVE", "L", "L", "lGripper", "bridge_lcd_CO2"],
+    ["MOVE", "L", "L", "lGripper", "plate_on_lcd_1"],
+    ["LdropPlateOnLCD", "", "", "", ""],
+    ["MOVE", "L", "L", "lGripper", "bridge_lcd_CO2"],
+]
 
 for comm in demo_commands_pickNplace_plate:
-    print(yp.send(yp.gen_command(comm[0],comm[1],comm[2],comm[3],comm[4])))
+    print(yp.send(yp.gen_command(comm[0], comm[1], comm[2], comm[3], comm[4])))
     time.sleep(0.5)
 
 print("demo executed")
