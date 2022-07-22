748c56dfdd34044383f9d8de35731bc93a7bfdeb
Manna Riddha <riddha.manna@epfl.ch>
2022-07-22 16:03:51 +0200

    reformatted

--- client.py
+++ client.py
@@ -1,16 +1,16 @@
 import socket
 
-HOST = '192.168.125.1'  # The server's hostname or IP address
-PORT = 4646        # The port used by the server
+HOST = "192.168.125.1"  # The server's hostname or IP address
+PORT = 4646  # The port used by the server
 
 
 while True:
     val = input("enter command: ")
-    if(val == 'quit'):
+    if val == "quit":
         break
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.connect((HOST, PORT))
-    s.sendall(bytes(val,'utf-8'))
+    s.sendall(bytes(val, "utf-8"))
     data = s.recv(1024)
-    print('Received', repr(data))
+    print("Received", repr(data))
     s.close()
